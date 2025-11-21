23a931d4baffacd7d1c4aaceb5258de0eb943ea0
Manan@2904 <mananp294@gmail.com>
2025-11-21 20:55:29 +0530

    first commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div className='w-full h-[200vh]'>
+            <div className='picture w-full h-full'>
+                <img className='w-full h-full object-cover' src="https://a.storyblok.com/f/133769/1920x2716/5c24d6b467/exo-ape-hero-1.jpg/m/2400x3395/filters:quality(90)" alt="" />
+            </div>
+        </div>
+
+    )
+}
+
+export default Landing
