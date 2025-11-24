07041f9e2ca045357d3edb625e9ddd7353e54194
Manan@2904 <mananp294@gmail.com>
2025-11-24 20:42:38 +0530

    updated news

--- src/components/Spread.jsx
+++ src/components/Spread.jsx
@@ -4,7 +4,7 @@ const Spread = () => {
   return (
     <div className='w-full h-[65vh] sm:h-[100vh] sm:py-20'>
 
-<div className='flex flex-col items-center justify-between gap-9 py-10'>
+<div className='flex flex-col items-center justify-between sm:gap-9 py-10'>
 
 <div className='flex gap-3 items-center justify-center'>
     <svg viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-4" data-v-669b4a84=""><path d="M7.41908 4.56679L6.13722 0L4.85418 4.92566L0 6L4.85418 7.25435L6.13722 12L7.3276 7.25435L12 6L7.41908 4.56679Z" fill="currentColor" data-v-669b4a84=""></path></svg>
